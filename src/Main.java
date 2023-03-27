ba20a1fe3795f9afdea4862ac4de34251b639879
klimchess <vanyusha.1996@mail.ru>
2023-03-27 23:16:36 +0300

    сделал первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+       Predicate<Integer> predicate = new Predicate<Integer>() {
+           @Override
+           public boolean test(Integer integer) {
+               if (integer < 0){
+                   return false;
+               }
+               return true;
+           }
+       };
+       Predicate<Integer> predicate1 = x -> x > 0;
+       System.out.println(predicate.test(-100));
+       System.out.println(predicate.test(100));
     }
 }
